9698e7cef45de79b1935fe3dc1f397eb4a20a813
Carlos Franco <kadusa09@gmail.com>
2024-11-11 13:32:39 -0300

    feature/Mediator

--- src/model/Mediator.java
+++ src/model/Mediator.java
@@ -1,6 +1,14 @@
 package model;
 
 public class Mediator {
-    // Implemente a classe Mediator que gerencia as trocas de itens entre jogadores.
-    // Crie o metodo exchangeItem para transferir itens de um jogador para outro.
+    public void exchangeItem(Player from, Player to, Item item, int quantity) {
+        if (from.getInventory().hasItem(item.getName())) {
+            from.getInventory().removeItem(item.getName(), quantity);
+            Item transferItem = new Item(item.getName(), quantity, item.getRarity(), item.getType());
+            to.getInventory().addItem(transferItem);
+            System.out.println("Troca concluida: " + item.getName() + " x" + " de " + from.getName() + " para " + to.getName());
+        } else {
+            System.out.println(from.getName() + " não existe mais " + item.getName() + " no inventario" );
+        }
+    }
 }
